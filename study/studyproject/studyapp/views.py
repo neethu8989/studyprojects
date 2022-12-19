f49913b70421f804830d2c8999db75a877ee5093
neethu8989 <nikithafahanas@gmail.com>
2022-12-19 15:32:50 +0300

    commit

---
+++ study/studyproject/studyapp/views.py
@@ -0,0 +1,15 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def demo(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return HttpResponse("hiking")
